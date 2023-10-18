f4555bfc423b8bc8e5d3842116f38e4ac1d0bba0
Harshvardhan <93473274+harshvardhan8058@users.noreply.github.com>
2023-10-18 16:02:13 +0530

    Update 1_december_gfg.cpp

--- 1_december_gfg.cpp
+++ 1_december_gfg.cpp
@@ -10,6 +10,14 @@ public:
     {
 
         // Your code here
+        int k = 0, j = n - 1, max = arr[n - 1] + 1;
+
+        for (int i = 0; i < n; i += 2)
+            arr[i] += (arr[j--] % max) * max;
+        for (int i = 1; i < n; i += 2)
+            arr[i] += (arr[k++] % max) * max;
+        for (int i = 0; i < n; i++)
+            arr[i] /= max;
     }
 };
 
